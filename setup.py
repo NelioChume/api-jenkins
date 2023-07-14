3f8b324716d00b43a7d39d5c8bc837c13545cced
NelioChume <74329121+NelioChume@users.noreply.github.com>
2023-07-14 15:29:53 +0200

    new

--- setup.py
+++ setup.py
@@ -1,8 +1,36 @@
-from setuptools import setup
+def adicao(a, b):
+    return a + b
 
-setup(
-    name='meu_codigo',
-    version='1.0',
-    py_modules=['meu_codigo'],
-    scripts=['meu_codigo.py'],
-)
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: Divisão por zero!"
+
+print("Selecione a operação:")
+print("1. Adição")
+print("2. Subtração")
+print("3. Multiplicação")
+print("4. Divisão")
+
+escolha = input("Digite sua opção (1/2/3/4): ")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+if escolha == '1':
+    print(num1, "+", num2, "=", adicao(num1, num2))
+elif escolha == '2':
+    print(num1, "-", num2, "=", subtracao(num1, num2))
+elif escolha == '3':
+    print(num1, "*", num2, "=", multiplicacao(num1, num2))
+elif escolha == '4':
+    print(num1, "/", num2, "=", divisao(num1, num2))
+else:
+    print("Opção inválida!")
