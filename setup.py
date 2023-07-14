83f8c13cd4a88d35b9eb1c524df1f379c93331f5
NelioChume <74329121+NelioChume@users.noreply.github.com>
2023-07-14 15:59:49 +0200

    nuew

--- setup.py
+++ setup.py
@@ -13,24 +13,24 @@ def divisao(a, b):
     else:
         return "Erro: Divisão por zero!"
 
-print("Selecione a operação:")
-print("1. Adição")
-print("2. Subtração")
-print("3. Multiplicação")
-print("4. Divisão")
+# print("Selecione a operação:")
+# print("1. Adição")
+# print("2. Subtração")
+# print("3. Multiplicação")
+# print("4. Divisão")
 
-escolha = input("Digite sua opção (1/2/3/4): ")
+# escolha = input("Digite sua opção (1/2/3/4): ")
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+# num1 = float(input("Digite o primeiro número: "))
+# num2 = float(input("Digite o segundo número: "))
 
-if escolha == '1':
-    print(num1, "+", num2, "=", adicao(num1, num2))
-elif escolha == '2':
-    print(num1, "-", num2, "=", subtracao(num1, num2))
-elif escolha == '3':
-    print(num1, "*", num2, "=", multiplicacao(num1, num2))
-elif escolha == '4':
-    print(num1, "/", num2, "=", divisao(num1, num2))
-else:
-    print("Opção inválida!")
+# if escolha == '1':
+#     print(num1, "+", num2, "=", adicao(num1, num2))
+# elif escolha == '2':
+#     print(num1, "-", num2, "=", subtracao(num1, num2))
+# elif escolha == '3':
+#     print(num1, "*", num2, "=", multiplicacao(num1, num2))
+# elif escolha == '4':
+#     print(num1, "/", num2, "=", divisao(num1, num2))
+# else:
+#     print("Opção inválida!")
--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Helloo")
+import setup as setup
+
+def test_add():
+    assert setup.adicao(2, 3) == 5
+
+def test_sub():
+    assert setup.subtracao(2, 3) == -1
+
+def tes_mult():
+    assert setup.multiplicacao(2, 3) == 6
+
+def test_div():
+    assert setup.subtracao(10, 5) == 2
